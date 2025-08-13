aa0d109da117493fc918d6d943c4bf52e4fd2d1f
avlsv <avlasov@nes.ru>
2025-08-13 10:32:55 +0300

    slides mistakes corrected

--- Code/Example_w_AAPL_options.R
+++ Code/Example_w_AAPL_options.R
@@ -10,89 +10,72 @@ library(patchwork)
 options(mc.cores = parallel::detectCores())
 
 
-
 estimation_procedure <- function(dataset,
                                  model = "simplex.stan",
                                  states = seq(120 - 20, 260 + 20, by = 10),
                                  iter = 4000,
                                  chains = 4) {
   print(dataset$date[1])
-  
+
   dataset_1 <- dataset |> select(expiration, strike, call_put, bid, ask)
-  
-  
-  
-  
+
+
+
+
   dataset_2 <-
     dataset_1 |>
     mutate(option_price = (bid + ask) / 2) |>
     select(-bid, -ask)
-  
-  
-  
-  
+
+
+
+
   expirations <- unique(dataset_2$expiration)
-  
-  current_prices <- aapl |>
-    filter(date == dataset$date[1]) |>
-    select(-date) |>
-    mutate(count = (length(expirations))) |>
-    uncount(count) |>
-    mutate(
-      call_put = "Call",
-      strike = 0,
-      expiration = expirations,
-      option_price = price
-    ) |>
-    select(-price)
-  
-  
-  
+
+
   results <- vector("list", length(expirations))
-  
-  dataset_2.1 <- bind_rows(dataset_2, current_prices)
-  
-  
+
+
   for (j in seq(1:length(expirations))) {
     print(expirations[j])
-    
+
     dataset_2.5 <-
       dataset_2 |>
       filter(expiration == expirations[j])
-    
-    
+
+
     states_vec <- as.vector(states)
-    
+
     states_tbl <-
       tibble(state = states_vec)
-    
-    
+
+
     dataset_3 <-
       cross_join(states_tbl, dataset_2.5) |>
       mutate(
         payoff =
           as.numeric(call_put == "Call") * pmax(state - strike, 0) +
-          as.numeric(call_put == "Put") * pmax(strike - state, 0)
+            as.numeric(call_put == "Put") * pmax(strike - state, 0)
       ) |>
       select(-expiration)
-    
+
     dataset_4 <-
       dataset_3 |>
       pivot_wider(names_from = state, values_from = payoff) |>
       select(-strike, -call_put)
-    
-    
+
+
     X_tbl <- dataset_4 |> select(-option_price)
     y_tbl <- dataset_4 |> select(option_price)
-    
-    
+
+
     stan_data_aapl <- list(
       n = nrow(X_tbl),
       k = ncol(X_tbl),
       X = X_tbl |> as.data.frame(),
       y = as.vector(y_tbl$option_price)
     )
-    
+
     stan_model_aapl <-
       stan(
         as.character(model),
@@ -100,58 +83,70 @@ estimation_procedure <- function(dataset,
         iter = iter,
         chains = chains
       )
-    
+
     coefs_90 <- stan_model_aapl |>
-      tidy(conf.int = T,
-           conf.level = 0.90,
-           conf.method = "HPDinterval")
-    
-    
+      tidy(
+        conf.int = T,
+        conf.level = 0.90,
+        conf.method = "HPDinterval"
+      )
+
+
     coefs_95 <- stan_model_aapl |>
-      tidy(conf.int = T,
-           conf.level = 0.95,
-           conf.method = "HPDinterval") |>
-      rename(conf.low.hpd.0.95 = conf.low,
-             conf.high.hpd.0.95 = conf.high)
-    
+      tidy(
+        conf.int = T,
+        conf.level = 0.95,
+        conf.method = "HPDinterval"
+      ) |>
+      rename(
+        conf.low.hpd.0.95 = conf.low,
+        conf.high.hpd.0.95 = conf.high
+      )
+
     coefs_50 <- stan_model_aapl |>
-      tidy(conf.int = T,
-           conf.level = 0.50,
-           conf.method = "HPDinterval") |>
+      tidy(
+        conf.int = T,
+        conf.level = 0.50,
+        conf.method = "HPDinterval"
+      ) |>
       rename(conf.low.hpd.50 = conf.low, conf.high.hpd.50 = conf.high)
-    
-    
+
+
     coefs_90_q <- stan_model_aapl |>
-      tidy(conf.int = T,
-           conf.level = 0.90,
-           conf.method = "quantile") |>
+      tidy(
+        conf.int = T,
+        conf.level = 0.90,
+        conf.method = "quantile"
+      ) |>
       rename(conf.low.q.90 = conf.low, conf.high.q.90 = conf.high)
-    
-    
-    
-    
+
+
+
+
     coefs <- coefs_90 |>
       left_join(coefs_95) |>
       left_join(coefs_50) |>
       left_join(coefs_90_q)
-    
-    
-    
+
+
+
     betas <- coefs |>
       filter(startsWith(term, "b")) |>
       mutate(state = states_tbl$state)
-    
+
     plot <- ggplot(betas, aes(x = state, y = estimate)) +
-      geom_col(color = "black",
-               fill = "gray",
-               alpha = 0.8) +
+      geom_col(
+        color = "black",
+        fill = "gray",
+        alpha = 0.8
+      ) +
       geom_errorbar(aes(max = conf.high, min = conf.low), width = 4) +
       scale_y_continuous("", breaks = extended_breaks(n = 6)) +
       scale_x_continuous("State", breaks = extended_breaks(n = round(length(states) / 2) + 1)) +
       theme_light()
-    
-    
-    results[[j]] <- list(stan_model_aapl, coefs, betas, plot)
+
+
+    results[[j]] <- list("model" = stan_model_aapl, "coefs" = coefs, "betas" = betas, "plot" = plot)
     results
   }
   return(results)
@@ -176,20 +171,20 @@ chains <- 4
 {
   start.time <- Sys.time()
   start.time
-  
+
   results_25 <-
     estimation_procedure(
-      dataset = 
+      dataset =
         read_csv("data/AAPL options 2025-03-25.csv", show_col_types = F),
       states = state_space,
-      iter = iter,
+      iter = 10,
       chains = chains
     )
-  
-  saveRDS(results_25, file = "Data/Results/results_25.RData")
-  
+
+  # saveRDS(results_25, file = "Data/Results/results_25.RData")
+
   end.time <- Sys.time()
-  
+
   time.taken <- end.time - start.time
   time.taken
 }
@@ -198,7 +193,7 @@ chains <- 4
 
 results_26 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-03-26.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -210,7 +205,7 @@ saveRDS(results_26, file = "Data/Results/results_26.RData")
 
 results_27 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-03-27.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -223,7 +218,7 @@ saveRDS(results_27, file = "Data/Results/results_27.RData")
 
 results_28 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-03-28.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -236,7 +231,7 @@ saveRDS(results_28, file = "Data/Results/results_28.RData")
 
 results_31 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-03-31.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -251,7 +246,7 @@ saveRDS(results_31, file = "Data/Results/results_31.RData")
 
 results_01 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-01.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -283,7 +278,7 @@ saveRDS(results_01, file = "Data/Results/results_01.RData")
 
 results_02 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-02.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -295,7 +290,7 @@ saveRDS(results_02, file = "Data/Results/results_02.RData")
 
 results_03 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-03.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -307,7 +302,7 @@ saveRDS(results_03, file = "data/results/results_03.RData")
 
 results_04 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-04.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -345,7 +340,7 @@ saveRDS(results_08, file = "data/results/results_08.RData")
 
 results_09 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-09.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -357,7 +352,7 @@ saveRDS(results_09, file = "data/results/results_09.RData")
 
 results_10 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-10.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -369,7 +364,7 @@ saveRDS(results_10, file = "data/results/results_10.RData")
 
 results_11 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-11.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -381,7 +376,7 @@ saveRDS(results_11, file = "data/results/results_11.RData")
 
 results_14 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-14.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -394,8 +389,9 @@ saveRDS(results_14, file = "data/results/results_14.RData")
 
 results_15 <-
   estimation_procedure(
-    dataset = 
-      read_csv("data/AAPL options 2025-04-15.csv", show_col_types = F),
+    dataset =
+      read_csv("data/AAPL options 2025-04-15.csv",
+               show_col_types = F),
     states = state_space,
     iter = iter,
     chains = chains
@@ -406,7 +402,7 @@ saveRDS(results_15, file = "data/results/results_15.RData")
 
 results_16 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-16.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -417,7 +413,7 @@ saveRDS(results_16, file = "data/results/results_16.RData")
 
 results_17 <-
   estimation_procedure(
-    dataset = 
+    dataset =
       read_csv("data/AAPL options 2025-04-17.csv", show_col_types = F),
     states = state_space,
     iter = iter,
@@ -425,9 +421,6 @@ results_17 <-
   )
 
 saveRDS(results_17, file = "data/results/results_17.RData")
-
-
-
 
 
 
--- Code/simplex.stan
+++ Code/simplex.stan
@@ -10,10 +10,16 @@ parameters {
   real<lower=0> alpha; // parameter for dirichlet distribution
   real<lower=0, upper=1> lambda; // discounting parameter
 }
+
+transformed parameters{
+  vector[n] f;
+  f = lambda * X * b;
+}
+
 model {
   //lambda ~ uniform(0.9,1);//lambda prior
   alpha ~ weibull(1.1,1); // dirichlet parameter prior
   b ~ dirichlet(rep_vector(alpha, k)); // coef prior
-  y ~ normal(lambda * X * b, sigma);  // likelihood
+  y ~ normal(f, sigma);  // likelihood
   
 }
